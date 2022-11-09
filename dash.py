f3a4719411a47a011e79dedbe57ddeab3e329f73
forzadata <aniel21@student.sdu.dk>
2022-11-09 15:32:31 +0100

    fix

--- dash.py
+++ dash.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 #import streamlit.components.v1 as html
-from  PIL import Image
+#from  PIL import Image
 import numpy as np
 import cv2
 import pandas as pd
