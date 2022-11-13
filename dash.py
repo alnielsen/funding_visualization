b83f0263c779c087699a251fbb20d9d2bc95cbc1
forzadata <aniel21@student.sdu.dk>
2022-11-13 19:51:19 +0100

    proof of concept [TILES]

--- dash.py
+++ dash.py
@@ -12,6 +12,8 @@ from streamlit_folium import st_folium
 import folium
 import random as rd
 import csv
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 
 # Set page configuration
@@ -36,12 +38,6 @@ omraade = []
 for i in df['Område']:
      if i not in omraade:
         omraade.append(i)
-
-
-
-
-#nums_dat.drop(nums_dat.filter(regex="Unname"),axis=1, inplace=True)
-
 
 
 
@@ -70,11 +66,8 @@ with st.sidebar:
 
 
 
-
-
-
 #### Function for displaying map ####
-def display_map(location, parameter):
+def display_map(location):#, parameter):
     #### Create folium map markers: ####
     au = folium.Marker(
                 location=[56.166666 , 10.1999992],
@@ -84,14 +77,14 @@ def display_map(location, parameter):
 
     ruc = folium.Marker(
                 location=[55.652330724, 12.137999448],
-                popup=f"Roskilde University  -{parameter}",
+                popup=f"Roskilde University",
                 icon=folium.Icon(color="black", icon="home"),
                 )
 
     ku = folium.Marker(
             location=[55.674497302, 12.570164386],
             prefix='fa',
-            popup=f'Copenhagen Universitet  -{parameter}',
+            popup=f'Copenhagen Universitet',
             icon=folium.Icon(icon='home', color='red'),
             
             )
@@ -122,7 +115,36 @@ def display_map(location, parameter):
         au.add_to(map)
 
 
-    st_map = st_folium(map, width=900, height=450)
+    st_map = st_folium(map, width=700, height=450)
+
+#### Histogram function ####
+def histo_chart():
+
+                arr = [x for x in range(500)]
+                fig1, ax1 = plt.subplots()
+                ax1.hist(arr, bins=20)
+                st.pyplot(fig1)
+
+
+#### Bankey chart function ####
+def bankey_chart():
+    fig3 = go.Figure(data=[go.Sankey(
+                    node = dict(
+                    pad = 15,
+                    thickness = 20,
+                    line = dict(color = "white", width = 0),
+                    label = ["A1", "A2", "B1", "B2", "C1", "C2"],
+                    color = "blue"
+                    ),
+                    link = dict(
+                    source = [0, 1, 0, 2, 3, 3], # indices correspond to labels, eg A1, A2, A1, B1, ...
+                    target = [2, 3, 3, 4, 4, 5],
+                    value = [8, 4, 2, 8, 4, 2]
+                ))])
+
+    fig3.update_layout(title_text="Basic Sankey Diagram", font_size=20)
+
+    st.plotly_chart(fig3)
 
 
 
@@ -132,60 +154,56 @@ def dashboard():
         st.title("Danmarks Frie Forskningsfond")
         st.subheader("Visualization of funding data & funding flows")
 
-       
-        dashcol1, dashcol2 = st.columns([1,4])
+       ## Create upper filter columns
+        dashcol1, dashcol2, dashcol3, dashcol4 = st.columns([1,1,1,1])
+
         with dashcol1:
 
-            chart_select = st.selectbox("Select a graph/chart", ("Overview", "Map", "Heatmap", "Sankey Chart", "Histogram"))
+            locations = st.selectbox("Choose an institution", (institution))
+
+        with dashcol2:
+
+            theme = st.selectbox("Choose a theme", (omraade))
+        with dashcol3:
+
+            chart_select3 = st.selectbox("filter3", ("Overview", "Map", "Heatmap", "Sankey Chart", "Histogram"))
+        with dashcol4:
+
+            chart_select4 = st.selectbox("filter4", ("Overview", "Map", "Heatmap", "Sankey Chart", "Histogram"))
     
         st.markdown("***")
 
-        
 
-        ## Choosing a visualization ##
-        
-        ## Add configuration to map ##
-        if chart_select == "Overview":
-             ## Create columns for page split ##
-            maincol, datacol = st.columns([2,5])
+        ## Create columns for chart split ##
+        mapcol, datacol = st.columns([1,1])
 
-            with maincol:
+        histocol, flowcol = st.columns([1,1])
 
+        ## MAP CHART ##
+        with mapcol:
+            display_map(locations)
 
 
-                locations = st.selectbox("Choose an institution", (institution))
+        ## DATA CHART ##
+        with datacol:
+            fig2 = st.dataframe(df.loc[(df['Institution'] == locations)])
+            source = 'Soruce: [Danmarks Frie Forskningsfond](https://dff.dk/forskningsprojekter)'
+            st.markdown(source, unsafe_allow_html=True)
+            #par_select = st.selectbox("Select a parameter for funding", ("Danish Crowns (DKK)", "Percentage (%)"))
 
-                theme = st.selectbox("Choose a theme", (omraade))
+        ## HISTORGRAM ##
+        with histocol:
+            histo_chart()
 
-                #par_select = st.selectbox("Select a parameter for funding", ("Danish Crowns (DKK)", "Percentage (%)"))
+        ## SANKEY CHART ##
+        with flowcol:
+            bankey_chart()
 
-
-            with datacol:
-
-                st.dataframe(df.loc[(df['Institution'] == locations)])
-                source = 'Soruce: [Danmarks Frie Forskningsfond](https://dff.dk/forskningsprojekter)'
-                st.markdown(source, unsafe_allow_html=True)
+            
 
 
                 
-        
-
-        if chart_select == "Map":
-            ## Create columns for page split ##
-            maincol, mapcol = st.columns([2,3])
-        
-
-
-            with maincol:
-
-                locations = st.selectbox("Choose an institution", (institution))
-
-                par_select = st.selectbox("Select a parameter for funding", ("Danish Crowns (DKK)", "Percentage (%)"))
-
-            with mapcol:
-                display_map(locations, par_select)
-
-            #st.select_slider("Year", years)    
+     
 
         
         
