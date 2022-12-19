9e69d3680ba1b4815fe87356104d1969266fcbfd
gustavchristensen1995 <73381743+gustavchristensen1995@users.noreply.github.com>
2022-12-19 13:53:13 +0100

    kilde til graf fjernet

--- gustav/gustav_figs.py
+++ gustav/gustav_figs.py
@@ -66,7 +66,7 @@ def generateSankey(df, year, category_columns):
             color = colorlist_source_rgba
         ))])
 
-    fig.update_layout(title_text="Funding of Research Grants in " + str(year) + "<br>Source: <a href='https://dff.dk/'>Danmarks Frie Forskningsfond</a>",
+    fig.update_layout(title_text="Funding of Research Grants in " + str(year),
                         font_size=10, height = 1000, width = 800)
     return fig
 
